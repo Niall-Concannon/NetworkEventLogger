02ff9582674674edda48ee848332024b8c2ba549
NIALL CONCANNON <nconcannon556@gmail.com>
2025-08-30 15:06:30 +0100

    Added Basic Network Events

--- src/network/Main.java
+++ src/network/Main.java
@@ -3,8 +3,14 @@ package network;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Event Objects
+		NetworkEvent event1 = new NetworkEvent("INFO", "Connection Established");
+		NetworkEvent event2 = new NetworkEvent("WARNING", "Slow Response Time Detected");
+		NetworkEvent event3 = new NetworkEvent("ERROR", "Connection Lost");
 
+		System.out.println(event1);
+		System.out.println(event2);
+		System.out.println(event3);
 	}
 
 }
--- src/network/NetworkEvent.java
+++ src/network/NetworkEvent.java
@@ -1,10 +1,26 @@
 package network;
 
+import java.time.LocalDateTime;
+
 public class NetworkEvent {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// Variables
+	private String eventType;
+	private String description;
+	private String time;
 
+	// Default Constructor
+	public NetworkEvent(String eventType, String description) {
+		this.eventType = eventType;
+		this.description = description;
+		
+		// Replaced the "T" added in LocalDateTime and cut off milliseconds
+		this.time = LocalDateTime.now().toString().replace("T", " ").substring(0, 19);
+	}
+
+	@Override
+	public String toString() {
+		return "Event Type: " + eventType + " | Description: " + description + " | Time: " + time;
 	}
 
 }
