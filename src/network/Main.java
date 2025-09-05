8c15ae0e26f17ef2cf73a0f0fca1d90d8c3938cf
NIALL CONCANNON <nconcannon556@gmail.com>
2025-09-05 14:16:04 +0100

    Added Random Events and Descriptions

--- src/network/Main.java
+++ src/network/Main.java
@@ -1,16 +1,39 @@
 package network;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// Event Objects
-		NetworkEvent event1 = new NetworkEvent("INFO", "Connection Established");
-		NetworkEvent event2 = new NetworkEvent("WARNING", "Slow Response Time Detected");
-		NetworkEvent event3 = new NetworkEvent("ERROR", "Connection Lost");
+		// Loop 5 Random Event Objects
+		for(int i = 0; i < 5; i++) {
+			NetworkEvent randomEvent = generateRandomEvent();
+			System.out.println(randomEvent);
+		}
+	}
 
-		System.out.println(event1);
-		System.out.println(event2);
-		System.out.println(event3);
+	// Method to get random events
+	public static NetworkEvent generateRandomEvent() {
+		// Event Types and Descriptions
+		String[] types = { "INFO", "WARNING", "ERROR" };
+		String[] infoDescriptions = { "Connection Established", "Network Settings Updated", "HTTP Request Made" };
+		String[] warningDescriptions = { "Slow Response Time Detected", "High Bandwidth Usage", "DNS Response Timeout" };
+		String[] errorDescriptions = { "Connection Lost", "DHCP Failure", "Checksum Error" };
+		
+		Random rand = new Random();
+		String type = types[rand.nextInt(types.length)];
+		String description = "";
+		if(type == "INFO") {
+			description = infoDescriptions[rand.nextInt(infoDescriptions.length)];
+		}
+		else if(type == "WARNING") {
+			description = warningDescriptions[rand.nextInt(warningDescriptions.length)];
+		}
+		else if(type == "ERROR") {
+			description = errorDescriptions[rand.nextInt(errorDescriptions.length)];
+		}
+		
+		return new NetworkEvent(type, description);
 	}
 
 }
